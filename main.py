29dd35706f2802f6791c5074eb71c4d9f962b5f9
joao9856 <joao-mm-pereira@hotmail.com>
2025-01-05 17:19:46 +0000

    added rotation

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        
+        player.update(dt)
         screen.fill("black")
+        
         player.draw(screen)
         pygame.display.flip()
 
